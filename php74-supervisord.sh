55204fa5556a394012acf69d70bb2cea821863a1
justamoose <31937522+justamoose@users.noreply.github.com>
2020-07-07 02:02:08 +0300

    Update php74-supervisord.sh

--- php74-supervisord.sh
+++ php74-supervisord.sh
@@ -3,7 +3,8 @@ sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.
 sudo yum -y install https://rpms.remirepo.net/enterprise/remi-release-7.rpm
 sudo yum -y install yum-utils
 sudo yum-config-manager --enable remi-php74
-sudo yum -y update 
+sudo yum -y update
+sudo yum -y install nano
 sudo yum -y install php php-cli 
 sudo yum -y install php php-cli php-fpm php-mysqlnd php-zip php-devel php-gd php-mcrypt php-mbstring php-curl php-xml php-pear php-bcmath php-json
 php -v
