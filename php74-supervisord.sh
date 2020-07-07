137fc32e64d5de4e9dba5bbbaa005806431f0ed3
justamoose <31937522+justamoose@users.noreply.github.com>
2020-07-08 00:56:27 +0300

    Update php74-supervisord.sh

--- php74-supervisord.sh
+++ php74-supervisord.sh
@@ -6,6 +6,8 @@ sudo yum-config-manager --enable remi-php74
 sudo yum -y update
 sudo yum -y install nano
 sudo yum -y install htop
+sudo yum -y install zip
+sudo yum -y install unzip
 sudo yum -y install php php-cli 
 sudo yum -y install php php-cli php-fpm php-mysqlnd php-zip php-devel php-gd php-mcrypt php-mbstring php-curl php-xml php-pear php-bcmath php-json
 php -v
